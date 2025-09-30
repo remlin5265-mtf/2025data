288a3a7bdaeefdbada7f46fc36c87f1c8fdec263
remlin5265-mtf <remlin5265@gmail.com>
2025-09-30 15:17:57 +0800

    Add files via upload

---
+++ avg.js
@@ -0,0 +1,13 @@
+function avg(score){
+    var sum=0, average=0;
+    for(var i-0;i<score.length;i++){
+        sum += score[i]
+        }
+        if(score.length>0)
+            average=sum/score.length;
+        return average;
+    }
+    //Test
+    //var Score=[50,20,30,15,16,21,33,11,-9,27];
+    var Score=[];
+    console. log(Score.toString="average is"+avg(Score));
